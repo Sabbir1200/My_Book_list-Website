b1350b15b6e8ce89d6b0d1b34f95488a03e2b580
4mahbubur4 <mahbubur.mr4@gmail.com>
2025-12-09 23:58:24 +0600

    added tostyfy in react

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoreDB } from '../../Utilites/AddToDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content';
+  import { ToastContainer, toast } from 'react-toastify';
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const data = useLoaderData();
     const bookId = parseInt(id);
-    const singleBook = data.find(book =>book.bookId === bookId);
-    const {bookName, image} = singleBook;
+    const singleBook = data.find(book => book.bookId === bookId);
+    const { bookName, image } = singleBook || {};
 
-    const handleMarkAsRead = (id)=>{
+
+    const handleMarkAsRead = (id) => {
         // store with id
         // where to store
         // array or collection
         // if book already exist the show a alert 
         // if book not exist then push in the collection or array
+        // MySwal.fire({
+        //     title: "Drag me!",
+        //     icon: "success",
+        //     draggable: true
+        // });
+        toast("Wow so easy!");
+
         addToStoreDB(id);
 
     }
@@ -22,7 +35,8 @@ const BookDetails = () => {
         <div className='border-2 w-2/3 mx-auto'>
             <img className='w-[300px]' src={image} alt={bookName} />
             <h5>{bookName}</h5>
-            <button onClick={()=>handleMarkAsRead(id)} className='btn btn-accent'>Mar as Read</button>
+            <ToastContainer />
+            <button onClick={() => handleMarkAsRead(id)} className='btn btn-accent'>Mar as Read</button>
             <button className='btn btn-info ml-6'>Add to WishList</button>
         </div>
     );
